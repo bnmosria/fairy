c89e8649d669fa1cd687ee3e07c33f48f3432fc4
Benjamin Osoria Peralta <bnmosria@hotmail.com>
2023-12-27 01:09:35 +0100

    Add repository test. Fix not running tests

---
+++ persistence/src/test/java/com/bopera/pointofsales/TestApplication.java
@@ -0,0 +1,7 @@
+package com.bopera.pointofsales;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+}
---
+++ persistence/src/test/java/com/bopera/pointofsales/persistence/repository/UserRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.bopera.pointofsales.persistence.repository;
+
+import com.bopera.pointofsales.persistence.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+@EnableJpaRepositories
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        User user1 = User.builder()
+            .username("admin1")
+            .password("password1")
+            .active(1)
+            .build();
+        User user2 = User.builder()
+            .username("admin2")
+            .password("password2")
+            .active(0)
+            .build();
+
+        entityManager.persist(user1);
+        entityManager.persist(user2);
+        entityManager.flush();
+    }
+
+    @Test
+    public void shouldFindTheLoginUsersOfActiveUsers() {
+        Optional<List<User>> userList = userRepository.findLoginUserList();
+        Assertions.assertTrue(userList.isPresent());
+        Assertions.assertEquals(1, userList.get().size());
+    }
+
+    @Test
+    public void shouldReturnsAnUserByUsername() {
+        boolean exists = userRepository.existsByUsername("admin1");
+        Assertions.assertTrue(exists);
+    }
+}
